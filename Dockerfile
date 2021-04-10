baf548c1b5e83541bc565d2a0671c3406f568f49
kyoowon <kyoowon95@gmail.com>
2021-04-10 15:43:20 +0900

    포그라운드로 컨테이너 유지

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 #1. debian:buster
-# FROM는 베이스가 될 이미지를 설정.
+# FROM는 베이스가 될 이미지를 설정. debian의 최신 안정화 버전을 buster로 지칭.
 FROM    debian:buster
 # MAINTAINER는 이미지를 생성한 사람의 정보를 설정. MAINTAINER 생성자가 지양하도록 변경되어 LABEL을 통해 maintainer를 지정.
 LABEL   MAINTAINER="kyulee@student.42Seoul.kr"
@@ -18,21 +18,21 @@ RUN		apt-get -y install \
 		php-mysql \
 		php-mbstring
 
-# wget를 이용한 파일 다운시 필요(phpmyadmin & wordpress)
+# wget를 이용한 파일 다운시 필요(phpmyadmin & wordpress) 빠른 실행을 위해 소스를 미리 다운.
 # RUN	    apt-get -y install wget \
 # 			vim
 
-#3. src 디렉토리에서 필요한 파일들을 복사
+#3. src 디렉토리에서 필요한 파일들을 복사.
 COPY    ./srcs/* /tmp/srcs/
 
 #4. run
-# 호스트와 바인딩할 포트를 지정
+# 호스트와 바인딩할 포트를 지정.
 EXPOSE  80 443
 
-# # 컨테이너 실행시 작업디렉토리를 지정
+# 컨테이너 실행시 주로 작업디렉토리를 지정.
 WORKDIR /tmp/srcs/
 
-# WORKDIR에서 지정한 디렉토리에 있는 init.sh를 실행
+# WORKDIR에서 지정한 디렉토리에 있는 init.sh를 실행.
 CMD     bash init.sh
 
 
@@ -40,10 +40,14 @@ CMD     bash init.sh
 # dockerfile로 빌드 (파일이 위치한 곳에서 다음 명령어 실행)
 # - docker build -t ft_server .
 # docker run
-# - docker run -it --name test -p 80:80 -p 443:443 ft_server
+# - docker run -p 80:80 -p 443:443 ft_server
 
 # docker 컨테이너 삭제
 # - docker rm test
 # docker에 실행 중인 컨테이너 중지, 컨테이너와 이미지 모두 삭제
 # - docker stop $(docker ps -aq)
 # - docker system prune -a
+# process 찾기 (포트번호를 넣으면 해당 포트에 PID를 알 수 있음)
+# lsof -i :포트번호
+# 포트를 점유하고 있는 process kill
+# kill -9 [PID]
--- srcs/init.sh
+++ srcs/init.sh
@@ -1,55 +1,65 @@
 #!/bin/sh
 
+# 주로 이용할 공간에 유저 그룹 권한설정을 통해 접근.
 chown -R www-data:www-data /var/www/
 chmod -R 755 ../../var/www/
 
-# ssl 설정 - https에 대한 설정
+# ssl 설정 - https에 대한 설정.
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
 mv localhost.dev.key ../../etc/ssl/private/
 mv localhost.dev.crt ../../etc/ssl/certs/
+
+# ssl 권한설정.
 chmod 600 ../../etc/ssl/certs/localhost.dev.crt ../../etc/ssl/private/localhost.dev.key
 
-# nginx 설정
-cp -rp default ../../etc/nginx/sites-available/
+# nginx 설정.
+AUTO=`echo $`
+if [ $NOTAUTOINDEX -eq 1 ]; then
+        cp -rp NotAuto_default ../../etc/nginx/sites-available/default
+else
+        cp -rp default ../../etc/nginx/sites-available/default
+fi
 
-# Wordpress 설치 및 설정
+# Wordpress 설치
 # wget https://wordpress.org/latest.tar.gz
-# tar -xvf latest.tar.gz
-# mv wordpress/ ../../var/www/html/
 tar -xvf wordpress-5.7.tar.gz
 mv wordpress/ ../../var/www/html/
 
-# 유저 그룹 권한설정
+# 유저 그룹 권한설정. - wordpress
 chown -R www-data:www-data ../../var/www/html/wordpress
 
-# wp-config.php 파일 수정
+# wp-config.php 파일 수정.
 cp -rp my-wp-config.php ../../var/www/html/wordpress/wp-config.php
 
-# mysql wordpress 테이블 및 권한 생성
+# mysql wordpress 테이블 및 권한 생성.
 service mysql start
 mysql -u root --skip-password < create_wordpress.sql
 
-# phpmyadmin 설치
+# phpmyadmin 설치.
 # wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
 mv phpmyadmin ../../var/www/html/
 
-cp -rp config.my.inc.php ../../var/www/html/phpmyadmin/config.inc.php 
+# phpmyadmin - mysql 접근을 위한 설정.
+cp -rp my-config.inc.php ../../var/www/html/phpmyadmin/config.inc.php 
 
-# 권한설정
+# 유저 그룹 권한설정. - phpmyadmin
 chown -R www-data:www-data ../../var/www/html/phpmyadmin/
 
-# 현재 위치를 root에 위치 & 설치한 파일과 폴더을 삭제
+# 현재 위치를 root에 위치 & 설치한 파일과 폴더을 삭제.
 cd ../../
-rm -rfp ./tmp/srcs
+rm -rf ./tmp/srcs
 
-# mysql 갱신
+# mysql 갱신.
 service mysql reload
-# nginx 실행
+# nginx 실행.
 service nginx start
-#php7.3-fpm 실행
+#php7.3-fpm 실행.
 service php7.3-fpm start
 
-# bash를 통해 컨테이너가 종료되지 않고 계속 남아 있을 수 있도록 함
-bash
+# while 통해 컨테이너가 종료되지 않고 포그라운드 방식으로 계속 남아 있을 수 있도록 함.
+while :
+do
+        sleep 100
+done
